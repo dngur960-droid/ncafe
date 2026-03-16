1dc4533d9be4e805fec9adcda37246cab6403c4f
woohyuk <woohyuk@ncafe.com>
2026-03-16 10:56:29 +0900

    fix(auth): Nginx Proxy 설정 수정 및 세션 Secure 옵션 생산환경 적용

--- frontend/app/lib/session.ts
+++ frontend/app/lib/session.ts
@@ -19,7 +19,7 @@ export const sessionOptions: SessionOptions = {
         httpOnly: true,
         // 배포 환경이 HTTPS가 아닐 경우 쿠키가 저장되지 않는 문제를 방지하기 위해 false로 설정하거나
         // 환경변수에 따라 유연하게 대처하도록 수정합니다.
-        secure: false, 
+        secure: process.env.NODE_ENV === 'production', 
         sameSite: 'lax' as const,
         path: '/',
         maxAge: 60 * 60 * 24, // 24시간
